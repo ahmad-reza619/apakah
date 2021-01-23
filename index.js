69b4db480e0a7017977e1e7c2b3898cb96da30e2
Reza <hungry.dev39@gmail.com>
2021-01-23 12:39:14 +0700

    add bool check

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const emptyParamMiddleware = callback => p => {
-  if (!p) {
+  if (typeof p === 'undefined') {
     throw new Error('no param provided');
   }
   return callback(p);
 }
+
 const string = emptyParamMiddleware(p => typeof p === 'string');
-const bool = p => typeof p === 'boolean';
-const func = p => typeof p === 'function';
-const num = p => typeof p === 'number';
-const obj = p => {
+const bool = emptyParamMiddleware(p => typeof p === 'boolean');
+const func = emptyParamMiddleware(p => typeof p === 'function');
+const num = emptyParamMiddleware(p => typeof p === 'number');
+const obj = emptyParamMiddleware(p => {
   const keyVal = Object.entries(p);
   return keyVal.every(([key, validator]) => {
     const isValid = validator(p[key]);
     return isValid;
   });
-}
+})
 
 module.exports = {
   string,
--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-const { string } = require('./index');
+const { string, bool } = require('./index');
 
 describe('string', () => {
   it('should return true if param is string', () => {
@@ -11,7 +11,23 @@ describe('string', () => {
     expect(string({})).toBe(false);
     expect(string([1, 2])).toBe(false);
   })
-  it('should return false if param is not provided', () => {
+  it('should throw if param is not provided', () => {
     expect(() => string()).toThrow();
+  })
+})
+
+describe('bool', () => {
+  it('should return true if param is bool', () => {
+    expect(bool(false)).toBe(true);
+  })
+  it('should return false if param is not bool', () => {
+    expect(bool(1)).toBe(false);
+    expect(bool('aaa')).toBe(false);
+    expect(bool(() => {})).toBe(false);
+    expect(bool({})).toBe(false);
+    expect(bool([1, 2])).toBe(false);
+  })
+  it('should throw if param is not provided', () => {
+    expect(() => bool()).toThrow();
   })
 })
