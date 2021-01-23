4a1c3ac75d024f302afce133fdee1643b2a42cee
Reza <hungry.dev39@gmail.com>
2021-01-23 12:27:27 +0700

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const string = p => {
+  typeof p === 'string';
+}
+const bool = p => typeof p === 'boolean';
+const func = p => typeof p === 'function';
+const num = p => typeof p === 'number';
+const obj = p => {
+  const keyVal = Object.entries(p);
+  return keyVal.every(([key, validator]) => {
+    const isValid = validator(p[key]);
+    return isValid;
+  });
+}
+
+module.exports = {
+  string,
+  obj,
+  bool,
+  func,
+  num,
+}
---
+++ index.spec.js
@@ -0,0 +1,17 @@
+const { string } = require('./index');
+describe('string', () => {
+  it('should return true if param is string', () => {
+    expect(string('wow')).toBe(true);
+  })
+  it('should return false if param is not string', () => {
+    expect(string(1)).toBe(false);
+    expect(string(true)).toBe(false);
+    expect(string(() => {})).toBe(false);
+    expect(string({})).toBe(false);
+    expect(string([1, 2])).toBe(false);
+    expect(string(null)).toBe(false);
+  })
+  it('should return false if param is not provided', () => {
+    expect(string()).toThrow();
+  })
+})
